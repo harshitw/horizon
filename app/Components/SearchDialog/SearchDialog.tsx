619d8d35e38a551825ec72d09ec06b32583d8a12
harshit awasthi <harshit.awasthi33@gmail.com>
2024-05-19 22:16:13 +0530

    Commit-2

--- app/Components/SearchDialog/SearchDialog.tsx
+++ app/Components/SearchDialog/SearchDialog.tsx
@@ -1,15 +1,26 @@
-'use client'
+"use client";
+import {
+  useGlobalContext,
+  useGlobalContextUpdate,
+} from "@/app/context/globalContext";
+import { commandIcon } from "@/app/utils/icons";
+import { Button } from "@/components/ui/button";
+import { Command, CommandInput } from "@/components/ui/command";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import React from "react";
 
-import { commandIcon } from '@/app/utils/icons'
-import { Button } from '@/components/ui/button'
-import { Command, CommandInput } from '@/components/ui/command'
-import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog'
-import React from 'react'
+function SearchDialog() {
+  const { geoCodedList, inputValue, handleInput } = useGlobalContext();
+  const { setActiveCityCoords } = useGlobalContextUpdate();
 
-const SearchDialog = () => {
+  const [hoveredIndex, setHoveredIndex] = React.useState<number>(0);
+
+  const getClickedCoords = (lat: number, lon: number) => {
+    setActiveCityCoords([lat, lon]);
+  };
   return (
-    <div className='search-btn'>
-        <Dialog >
+    <div className="search-btn">
+      <Dialog>
         <DialogTrigger asChild>
           <Button
             variant="outline"
@@ -18,23 +29,61 @@ const SearchDialog = () => {
             <p className="text-sm text-muted-foreground">Search Here...</p>
             <div className="command dark:bg-[#262626] bg-slate-200  py-[2px] pl-[5px] pr-[7px] rounded-sm ml-[10rem] flex items-center gap-2">
               {commandIcon}
-              <span className="text-[9px]">F</span> 
+              <span className="text-[9px]">F</span>
             </div>
           </Button>
         </DialogTrigger>
-        <DialogContent className='p-0'> 
-            <Command className='rounded-lg border shadow-md'>
-            <CommandInput placeholder="Type a command or search..."/>
-            <ul className='px-3 pb-2'>
-                <p className='p-2 text-sm text-muted-foreground '> 
-                    Suggestions
-                </p>
+
+        <DialogContent className="p-0">
+          <Command className=" rounded-lg border shadow-md">
+            <CommandInput
+              value={inputValue}
+              onChangeCapture={handleInput}
+              placeholder="Type a command or search..."
+            />
+            <ul className="px-3 pb-2">
+              <p className="p-2 text-sm text-muted-foreground">Suggestions</p>
+
+              {geoCodedList?.length === 0 ||
+                (!geoCodedList && <p>No Results</p>)}
+
+              {geoCodedList &&
+                geoCodedList.map(
+                  (
+                    item: {
+                      name: string;
+                      country: string;
+                      state: string;
+                      lat: number;
+                      lon: number;
+                    },
+                    index: number
+                  ) => {
+                    const { country, state, name } = item;
+                    return (
+                      <li
+                        key={index}
+                        onMouseEnter={() => setHoveredIndex(index)}
+                        className={`py-3 px-2 text-sm  rounded-sm cursor-default
+                        ${hoveredIndex === index ? "bg-accent" : ""}
+                      `}
+                        onClick={() => {
+                          getClickedCoords(item.lat, item.lon);
+                        }}
+                      >
+                        <p className=" text">
+                          {name}, {state && state + ","} {country}
+                        </p>
+                      </li>
+                    );
+                  }
+                )}
             </ul>
-            </Command>
+          </Command>
         </DialogContent>
-        </Dialog>
+      </Dialog>
     </div>
-  )
+  );
 }
 
-export default SearchDialog
+export default SearchDialog;
---
+++ app/utils/defaultStates.tsx
@@ -0,0 +1,39 @@
+const defaultStates = [
+    {
+        name : "Delhi",
+        country : "India",
+        state : "Delhi",
+        lat : 28.7041,
+        long : 77.1025,
+    },
+    {
+        name : "New York",
+        country : "US",
+        state : "New York",
+        lat : 40.7128,
+        long : 74.0060,
+    },
+    {
+        name : "Sydney",
+        country : "Australia",
+        state : "Sydney",
+        lat : 33.8688,
+        long : 151.2093,
+    },
+    {
+        name : "London",
+        country : "UK",
+        state : "London",
+        lat : 51.5072,
+        long : 0.1276,
+    },
+    {
+        name : "Tokyo",
+        country : "Japan",
+        state : "Tokyo",
+        lat : 35.6764,
+        long : 139.6500,
+    }
+]
+
+export default defaultStates;
